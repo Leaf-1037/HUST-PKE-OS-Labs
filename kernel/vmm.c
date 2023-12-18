217ab12e474fc6c2e5b8895b030a1391ec6f1220
Leaf-1037 <2457537174@qq.com>
2023-12-18 22:16:01 +0800

    lab2_1 done.

--- kernel/vmm.c
+++ kernel/vmm.c
@@ -159,8 +159,9 @@ void *user_va_to_pa(pagetable_t page_dir, void *va) {
   // (va & (1<<PGSHIFT -1)) means computing the offset of "va" inside its page.
   // Also, it is possible that "va" is not mapped at all. in such case, we can find
   // invalid PTE, and should return NULL.
-  panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
-
+  //panic( "You have to implement user_va_to_pa (convert user va to pa) to print messages in lab2_1.\n" );
+  uint64 physical_address = lookup_pa(page_dir, ((uint64)va)) + ((unsigned long long)va & ((1<<PGSHIFT)-1));
+  return (void*)physical_address;
 }
 
 //
